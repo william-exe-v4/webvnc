5f82d5cac07e894b84fdbe8701e824293643a6b3
havocchan <wilsonxfeng@gmail.com>
2021-03-15 22:01:26 -0600

    got started on loginss
    I can prob finish in about 2 - 3 days if I learn how to do it

--- novnc/Website/Website.py
+++ novnc/Website/Website.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
+import os
 
+# idk how to do this i can prob do it tmr
 # program is the name of the app i cant think of any names lmfao
 program = Flask(__name__)
 
@@ -11,6 +14,5 @@ def index():
     return render_template("login.html")
 
 
-# if this file is the main file, do this
 if __name__ == '__main__':
     program.run(debug=True)
