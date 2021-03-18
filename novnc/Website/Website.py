f12bb0cb78cb6f3305790f622c2a6f5f13836967
havocchan <wilsonxfeng@gmail.com>
2021-03-18 17:39:21 -0600

    depreshu

--- novnc/Website/Website.py
+++ novnc/Website/Website.py
@@ -13,28 +13,27 @@ def login():
         if request.form.get('Login'):
             # Gets username from the input box
             username = request.form.get('username')
-            authedR = open('usernames.txt', 'r')
             error = "ERROR USERNAME IS NOT REGISTERED"
-            authorize = authedR.read()
             # if username from input box is in the text file 'usernames.txt'
-            if username in authorize:
-                return render_template('vnc.html')
-                # Paste the url in here
-                # return redirect("https://www.webnovnc.com", code=302)
-            else:
-                return error
+            with open("usernames.txt", "r") as txtFile:
+                for line in txtFile:
+                    if re.search(username, line):
+                        return render_template('vnc.html')
+                    else:
+                        return error
         elif request.form.get('Register'):
             # Gets the username from the input box
             username = request.form.get('regUsername')
-            regUserWrite = open('usernames.txt', 'a')
-            regUserRead = open('usernames.txt', 'r')
-            a = regUserRead.read()
-            error = "ERROR USERNAME IS ALREADY REGISTERED"
-            if username in a:
-                return error
-            else:
-                regUserWrite.write(username + '\n')
-                return render_template('login.html')
+            error = 'ERROR USERNAME ALREADY REGISTERED'
+            print(username)
+            with open("usernames.txt", "r") as txtFile:
+                for line in txtFile:
+                    if re.search(username, line):
+                        return error
+                    else:
+                        regUserA = open('usernames.txt', 'a')
+                        regUserA.write(username + '\n')
+                        return render_template('login.html')
 
     return render_template("login.html")
 
