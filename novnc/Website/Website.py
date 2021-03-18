65ded37e5fca80f2a21317d0984250e5b163929f
havocchan <wilsonxfeng@gmail.com>
2021-03-17 21:59:10 -0600

    I did functions for both buttons
    There's a major bug that I will prooooooooooooobably fix tmr. The bug is where u only need to match one letter and it will think that because the letter is in the file, it matches

--- novnc/Website/Website.py
+++ novnc/Website/Website.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-import time
+import re
 
 program = Flask(__name__)
 
@@ -9,19 +9,32 @@ program = Flask(__name__)
 @program.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
-        authedR = open('usernames.txt', 'r')
-        error = "ERROR USERNAME IS NOT REGISTERED"
-
-        authorize = authedR.read()
-        print(authorize)
-        if username in authorize:
-            return render_template('vnc.html')
-            # Paste the url in here
-            # return redirect("https://www.webnovnc.com", code=302)
-
-        else:
-            return error
+        # If the login button was pressed
+        if request.form.get('Login'):
+            # Gets username from the input box
+            username = request.form.get('username')
+            authedR = open('usernames.txt', 'r')
+            error = "ERROR USERNAME IS NOT REGISTERED"
+            authorize = authedR.read()
+            # if username from input box is in the text file 'usernames.txt'
+            if username in authorize:
+                return render_template('vnc.html')
+                # Paste the url in here
+                # return redirect("https://www.webnovnc.com", code=302)
+            else:
+                return error
+        elif request.form.get('Register'):
+            # Gets the username from the input box
+            username = request.form.get('regUsername')
+            regUserWrite = open('usernames.txt', 'a')
+            regUserRead = open('usernames.txt', 'r')
+            a = regUserRead.read()
+            error = "ERROR USERNAME IS ALREADY REGISTERED"
+            if username in a:
+                return error
+            else:
+                regUserWrite.write(username + '\n')
+                return render_template('login.html')
 
     return render_template("login.html")
 
